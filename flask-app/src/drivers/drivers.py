0db9c045798b389925682828fa048218827247f7
ben-weiss0 <bweiss1000@yahoo.com>
2023-04-15 10:40:50 -0400

    potential solution for adding new driver

--- flask-app/src/drivers/drivers.py
+++ flask-app/src/drivers/drivers.py
@@ -2,9 +2,9 @@ from flask import Blueprint, request, jsonify, make_response
 import json
 from src import db
 
-# DELETE will be firing a driver
+# -- DELETE will be firing a driver  done potentially
 
-# POST will be new driver joining the app joining app
+# -- POST will be new driver joining the app joining app  done potentially
 
 # PUT when driver changes desired radius
 # PUT for driver to change current location
@@ -30,6 +30,34 @@ def get_drivers():
 
     # use cursor to query the database for a list of products
     cursor.execute(query)
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+# Adds a driver to the database of drivers
+@drivers.route('/drivers/hireDriver', methods=['POST'])
+def get_drivers(employeeid, phone_number, radius, drivers_license,
+current_location, transportation):
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute("INSERT INTO Driver VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    (employeeid, phone_number, radius, drivers_license, 0, current_location, 0, transportation, 0))
 
     # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
