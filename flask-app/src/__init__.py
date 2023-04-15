5373cf463533ac9a2b5c19e489f83b06a9ee9000
ben-weiss0 <bweiss1000@yahoo.com>
2023-04-15 11:21:23 -0400

    updated init file

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'northwind'  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'hungerhero'  # Change this to your DB name
 
     # Initialize the database object with the settings above. 
     db.init_app(app)
@@ -27,16 +27,14 @@ def create_app():
     # Add a default route
     @app.route("/")
     def welcome():
-        return "<h1>Welcome to the 3200 boilerplate app</h1>"
+        return "<h1>Welcome to HungerHero</h1>"
 
     # Import the various routes
-    from src.views import views
-    from src.customers.customers import customers
-    from src.products.products  import products
+    from src.drivers.drivers import drivers
+    from src.users.users  import users
 
     # Register the routes that we just imported so they can be properly handled
-    app.register_blueprint(views,       url_prefix='/v')
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(drivers,   url_prefix='/d')
+    app.register_blueprint(users,    url_prefix='/u')
 
     return app
