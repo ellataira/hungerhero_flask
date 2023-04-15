81dfcf0f151dce89ff658e361752f466ca1a70e3
sofiecook <112681376+sofiecook@users.noreply.github.com>
2023-04-15 13:47:27 -0400

    Sofie's

--- flask-app/src/drivers/drivers.py
+++ flask-app/src/drivers/drivers.py
@@ -18,19 +18,15 @@ drivers = Blueprint('drivers', __name__)
 
 # Removes a driver from the database drivers
 @drivers.route('/fireDriver', methods=['DELETE'])
-def remove_driver():
+def remove_driver(employee_id):
     # get a cursor object from the database
     cursor = db.get_db().cursor()
-
-    data = request.json 
-
-    employee_id = data['employeeid']
 
     # this might not be right
     query = '''
         DELETE FROM Driver
-        WHERE employeeid = {}
-    '''.format(employee_id)
+        WHERE employeeid = employee_id
+    '''
 
     # use cursor to query the database for a list of products
     cursor.execute(query)
@@ -54,19 +50,14 @@ def remove_driver():
 
 # Adds a driver to the database of drivers
 @drivers.route('/hireDriver', methods=['POST'])
-def hire_driver():
+def hire_driver(employeeid, phone_number, radius, drivers_license,
+current_location, transportation):
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
-    data = request.json
-
-    employeeid, phone_number, radius, drivers_license, current_location, transportation = data['employeeid'], data['phone_number'], data['radius'], data['drivers_license'], data['current_location'], data['transportation']
-
     # use cursor to query the database for a list of products
-    # cursor.execute("INSERT INTO Driver VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-    # (employeeid, phone_number, radius, drivers_license, "0", current_location, "0", transportation, "0"))
-
-    cursor.execute("INSERT INTO Driver (employeeid, phone_number, radius, drivers_license, current_location, jobs_completed, transportation, total_earned, rating) VALUES ({}, {},{},{},{},{},{},{},{})".format(employeeid, phone_number, radius, drivers_license, "0", current_location, "0", transportation, "0"))
+    cursor.execute("INSERT INTO Driver VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    (employeeid, phone_number, radius, drivers_license, 0, current_location, 0, transportation, 0))
 
     # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
@@ -87,9 +78,13 @@ def hire_driver():
 
 # Allows a driver to update their desired working radius
 @drivers.route('/updateRadius', methods=['PUT'])
-def update_radius(new_radius, employee_id):
+def update_radius():
     # get a cursor object from the database
     cursor = db.get_db().cursor()
+    
+    data = request.json
+    
+    new_radius, employee_id = data["new_radius"], data["employee_id"]
 
     #maybe?
     query = '''
@@ -122,6 +117,11 @@ def update_radius(new_radius, employee_id):
 def update_location(new_location, employee_id):
     # get a cursor object from the database
     cursor = db.get_db().cursor()
+    
+        
+    data = request.json
+    
+    new_location, employee_id = data["new_location"], data["employee_id"]
 
     #maybe?
     query = '''
@@ -155,6 +155,11 @@ def update_location(new_location, employee_id):
 def new_order_completed(employee_id):
     # get a cursor object from the database
     cursor = db.get_db().cursor()
+    
+        
+    data = request.json
+    
+    employee_id = data["employee_id"]
 
     updated_earned = '''
         SELECT SUM(total_amount)
@@ -188,6 +193,7 @@ def new_order_completed(employee_id):
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
 
 # Get top 20 worst rated drivers
 @drivers.route('/worstDrivers', methods=['GET'])
